0bc90ac3b2c2704ca0efb72021418dbf96f8d41e
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-19 13:26:33 -0500

    add: show content using props

--- react_js_components/src/index.js
+++ react_js_components/src/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import faker from 'faker';
+
+import CommentDetail from './CommentDetail'
+import Faker from 'faker'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
+            <CommentDetail author="Dean" timeAgo="Today at 4:45PM" comment="I'm a hunter." avatar={Faker.image.avatar()} />
+            <CommentDetail author="Quan" timeAgo="Today at 2:00AM" comment="I love to watch anime." avatar={Faker.image.avatar()} />
+            <CommentDetail author="Maria" timeAgo="Yesterday at 5:00PM" comment="I also love to watch anime!" avatar={Faker.image.avatar()} />
         </div>
     )
 }
