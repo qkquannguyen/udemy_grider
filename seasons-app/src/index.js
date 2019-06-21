78d6fa21f5e4eb36eb0098d9a36d3662b4250562
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-21 15:41:38 -0500

    add: State Constructor

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -1,7 +1,23 @@
+/* eslint-disable no-useless-constructor */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+    // --- NOTE: Very first function to be called before anything else
+    constructor(props) {
+        // --- NOTE: It is required to call 'super()' and passing in props
+        // --- NOTE: 'super()' is a reference to the parent constructor function
+        super(props)
+
+        // --- NOTE: This is the state object
+        this.state = {
+            // --- NOTE: If you don't know the value yet, then default to 'null'
+            latitude: null
+        }
+    }
+
+    // --- NOTE: React says we have to define 'render'
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
