728f42fd3e84c7ef2e3c8d40242f4052cbd02958
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-24 13:43:29 -0500

    chore: SeasonDisplay modifications

--- seasons-app/src/SeasonDisplay.js
+++ seasons-app/src/SeasonDisplay.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+const getSeason = (latitude, month) => {
+    if (month > 2 && month < 9) {
+        return latitude > 0 ? 'summer' : 'winter'
+    } else {
+        return latitude > 0 ? 'winter' : 'summer'
+    }
+}
+
 const SeasonDisplay = (props) => {
-    return <div>Season Display!</div>
+    const season = getSeason(props.latitude, new Date().getMonth())
+    return <div>Season Display: {season}</div>
 }
 
 export default SeasonDisplay
--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay'
+
 class App extends React.Component {
 
-    // --- NOTE: Very first function to be called before anything else
-    constructor(props) {
-        // --- NOTE: It is required to call 'super()' and passing in props
-        // --- NOTE: 'super()' is a reference to the parent constructor function
-        super(props)
-
-        // --- NOTE: This is the state object
-        // !!! NOTE: This is the only time there will be a direct assignment of the state object
-        this.state = {
-            // --- NOTE: If you don't know the value yet, then default to 'null'
-            latitude: null,
-            errorMessage: ''
-        }
+    // NOTE: Another way to write your state
+    state = { 
+        latitude: null,
+        errorMessage: ''
     }
-
+    
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => {
@@ -34,6 +27,21 @@ class App extends React.Component {
             }
         )  
     }
+    // !!! INFO: Uncommment below to run the application using a constructor and super(props)
+    // --- NOTE: Very first function to be called before anything else
+    // constructor(props) {
+    //     // --- NOTE: It is required to call 'super()' and passing in props
+    //     // --- NOTE: 'super()' is a reference to the parent constructor function
+    //     super(props)
+
+    //     // --- NOTE: This is the state object
+    //     // !!! NOTE: This is the only time there will be a direct assignment of the state object
+    //     this.state = {
+    //         // --- NOTE: If you don't know the value yet, then default to 'null'
+    //         latitude: null,
+    //         errorMessage: ''
+    //     }
+    // }
 
     // --- NOTE: React says we have to define 'render'
     render() {
@@ -41,7 +49,7 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.latitude) {
-            return <div>Latitude: {this.state.latitude}</div>
+            return <SeasonDisplay latitude={this.state.latitude} />
         }
 
         return <div>Loading!</div>
