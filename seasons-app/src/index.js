fde13217c64eff698bfc12f1af417d060f991ded
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-24 10:32:53 -0500

    add: Component Notes

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -56,12 +56,13 @@ ReactDOM.render(
  * Components
  * ------------------------------------------------------------------------------------------------
  * --- Component Lifecycle
- * -------- constructor
- * -------- render
+ * -------- constructor                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ good for one time setup
+ * -------- render                          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Only JSX here
  * -------- *content will be visible on screen*
- * -------- componentDidMount
+ * -------- componentDidMount               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Good for data loading
  * -------- *sit and wait for updates*
- * -------- componentDidUpdate
+ * -------- *render() called*
+ * -------- componentDidUpdate              ~ Good for more data loading when state / props changes
  * -------- *sit and wait until this component is not longer shown*
- * -------- componentWillUnmount
+ * -------- componentWillUnmount            ~~~~~~~ Good place to do cleanup (Esp. non-react stuff)
  */
