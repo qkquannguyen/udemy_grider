b89776540444d2c3465691cdfe046d77cd8f8352
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-25 09:46:11 -0500

    chore: display icons depending on season

--- seasons-app/src/SeasonDisplay.js
+++ seasons-app/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        seasonText: 'Lets hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        seasonText: 'Burr it is cold!',
+        iconName: 'snowflake'
+    }
+}
+
 const getSeason = (latitude, month) => {
     if (month > 2 && month < 9) {
         return latitude > 0 ? 'summer' : 'winter'
@@ -10,10 +21,14 @@ const getSeason = (latitude, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.latitude, new Date().getMonth())
+    // --- NOTE: This pulls the seasonConfig object --> { seasonText, iconName } 
+    const { seasonText, iconName } = seasonConfig[season]
 
     return (
         <div>
-            {season === 'winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!'}
+            <i className={`${iconName} icon`}/>
+            <h1> {seasonText} </h1>
+            <i className={`${iconName} icon`}/>
         </div>
     )
 }
