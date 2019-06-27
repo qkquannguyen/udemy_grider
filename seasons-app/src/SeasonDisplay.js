49f2ce6ef629a44451037c6660ddfab16907a8e4
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-27 09:37:41 -0500

    add: CSS styling of the application

--- seasons-app/src/SeasonDisplay.js
+++ seasons-app/src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -25,10 +26,11 @@ const SeasonDisplay = (props) => {
     const { seasonText, iconName } = seasonConfig[season]
 
     return (
-        <div>
-            <i className={`${iconName} icon`}/>
+        // --- NOTE: A good convention is to make the root class name the same as your Component
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}/>
             <h1> {seasonText} </h1>
-            <i className={`${iconName} icon`}/>
+            <i className={`icon-right massive ${iconName} icon`}/>
         </div>
     )
 }
