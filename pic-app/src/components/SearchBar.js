6c541fb6e1c1377600c98470c53608ab559d33ed
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-28 12:08:20 -0500

    add: Text Input and SearchBar UI

--- pic-app/src/components/SearchBar.js
+++ pic-app/src/components/SearchBar.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
+
     render() {
         return  (
-            <div>
-                <form>
-                    <input type="text"/>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Waifu Search</label>
+                        {/* NOTE: There is no () at this method call because doing so, it will
+                            ----- call the method when our component is rendered. We dont want that
+                          */}
+                        <input type="text" onChange={this.onInputChange}/>
+                    </div>
                 </form>
             </div>
         )
