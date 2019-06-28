495af91de5254dcd0ef3ea48f0f91c215ebcd303
Quan Nguyen <qk.quannguyen@gmail.com>
2019-06-28 13:01:24 -0500

    fix: Property of Undeifne Error

--- pic-app/src/components/SearchBar.js
+++ pic-app/src/components/SearchBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    // --- NOTE: This is the way of making an uncontrolled Component
     // onInputChange(event) {
     //     console.log(event.target.value)
     // }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+    }
 
     state = {
         term: ''
@@ -12,13 +17,14 @@ class SearchBar extends React.Component {
     render() {
         return  (
             <div className="ui segment">
-                <form className="ui form">
+                <form 
+                    onSubmit={this.onFormSubmit} 
+                    className="ui form">
                     <div className="field">
                         <label>Waifu Search</label>
                         {/* NOTE: There is no () at this method call because doing so, it will
-                            ---s-- call the method when our component is rendered. We dont want that
-                          */}
-                        {/* <input type="text" onChange={this.onInputChange}/> */}
+                            ----- call the method when our component is rendered. We dont want that
+                        <input type="text" onChange={this.onInputChange}/> */}
                         <input 
                             type="text" 
                             value={this.state.term}
