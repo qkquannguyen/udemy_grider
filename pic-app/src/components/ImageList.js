1908d230681b785d02bf8e5d6f6541a94f7e434c
Quan Nguyen <qk.quannguyen@gmail.com>
2019-07-02 09:35:06 -0500

    add: CSS File for Image List

--- pic-app/src/components/ImageList.js
+++ pic-app/src/components/ImageList.js
@@ -1,3 +1,4 @@
+import './ImageList.css'
 import React from 'react'
 
 const ImageList = props => {
@@ -5,7 +6,7 @@ const ImageList = props => {
         // eslint-disable-next-line jsx-a11y/alt-text
         return <img alt={description} key={id} src={urls.regular}/>
     })
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList
