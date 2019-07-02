38a543934cbfca263ebb0862de674c3bf983c602
Quan Nguyen <qk.quannguyen@gmail.com>
2019-07-02 15:57:52 -0500

    chore: give search-bar a state object

--- youtube-app/src/components/App.js
+++ youtube-app/src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar'
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className="ui container">
                 NO WAIFU NO LAIFU
                 {() => {console.log("NO WAIFU NO LAIFU")}}
                 <SearchBar />
--- youtube-app/src/components/SearchBar.js
+++ youtube-app/src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    state = {
+        searchTerm: ''
+    }
+
+    onSearchInputChange = (searchTerm) => {
+        console.log(searchTerm)
+    }
+
     render() {
-        return <div> WAIFU SEARCH BAR </div>
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Waifu Video Search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.searchTerm}
+                            onChange={this.onSearchInputChange}
+                        />
+                    </div>
+                </form>
+                WAIFU SEARCH BAR 
+            </div>
+        )
     }
 }
 
