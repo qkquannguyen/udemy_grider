47da5c0732c355770fc6a5d9f42a438156ebd3e9
Quan Nguyen <qk.quannguyen@gmail.com>
2019-07-09 15:15:02 -0500

    chore: clean up direectory and styled app

--- waifu-list-app/src/components/WaifusList.js
+++ waifu-list-app/src/components/WaifusList.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+// --- NOTE: This is not really importing an action. More realistically,
+// --- this is simply importing an object
+import { selectWaifu } from '../actions'
+
 class WaifusList extends React.Component {
     renderWaifus() {
         return this.props.waifus.map((waifu) => {
             return (
                 <div className="item" key={waifu.name}>
                     <div className="right floated content">
-                        <button className="ui button primary">
+                        <button 
+                            className="ui button primary"
+                            onClick={() => this.props.selectWaifu(waifu)}
+                        >
                             Select
                         </button>
                     </div>
@@ -34,4 +41,8 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(WaifusList)
+export default connect(mapStateToProps, { selectWaifu })(WaifusList)
+
+// !!! NOTE: Redux is not magic!
+// --- Redux does not detect action creators being called and does not detect a function
+// --- returning an object that is an 'action'. Hence why the action was passed into connect()
