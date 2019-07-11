2f128d147555c35793a418103f69d1dcda2ec51c
Quan Nguyen <qk.quannguyen@gmail.com>
2019-07-11 14:24:11 -0500

    add: post reducer

--- blog-app/src/reducers/index.js
+++ blog-app/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postReducer from './postReducers';
 
 export default combineReducers({
-    waifu: () => 'Nino Nakano'
+    posts: postReducer
 });
---
+++ blog-app/src/reducers/postReducers.js
@@ -0,0 +1,15 @@
+// --- NOTE: Rules of Reducers
+// ---  1. Must return any value other than 'undefined'
+// ---  2. Produces 'state', or data to be used inside of your app using only previous state and
+// ------- the action (reducers are pure)
+// ---  3. Must not return 'out of itself' to decide what value to return
+// ---  4. Must not mutate its input 'state' argument
+// ------- However, the truth hurts. You can do this all, day every day.
+// ------- The one corner case s
+export default (state = [], action) => {
+    if (action.type === 'FETCH_POSTS') {
+        return action.payload;
+    }
+    
+    return state;
+};
