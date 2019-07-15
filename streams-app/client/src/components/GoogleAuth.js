5059070e927b57d3e10745ac72298f8cf2a230a1
Quan Nguyen <qk.quannguyen@gmail.com>
2019-07-15 15:22:33 -0500

    add: OAuth Sign In Status

--- streams-app/client/src/components/GoogleAuth.js
+++ streams-app/client/src/components/GoogleAuth.js
@@ -3,19 +3,50 @@ import React from 'react'
 import config from '../config'
 
 class GoogleAuth extends React.Component {
+    state = {
+        isSignedIn: null
+    }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: config.clientId,
                 scope: config.EMAIL
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({ 
+                    isSignedIn: this.auth.isSignedIn.get() 
+                })
             })
         })
     }
     
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return (
+                <div>
+                    I don't know if we are signed in.
+                </div>
+            )
+        } else if (this.state.isSignedIn) {
+            return (
+                <div>
+                    I am signed in!
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    I am not signed in!
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
-                Google Auth
+                {this.renderAuthButton()}
             </div>
         )
     }
