06e4c36eeb3b722068766f91d8d71d1964d77428
Quan Nguyen <qk.quannguyen@gmail.com>
2019-07-16 12:06:23 -0500

    add: form validation

--- streams-app/client/src/components/streams/StreamCreate.js
+++ streams-app/client/src/components/streams/StreamCreate.js
@@ -2,20 +2,57 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
-    renderInput(formProps) {
-        return <input {...formProps.input}/>
+    renderInput({ input, label}) {
+        return (
+            <div className="field">
+                <label>
+                    {label}
+                </label>
+                <input {...input}/>
+            </div>
+        )
+    }
+
+    onSubmit(formValues) {
+        console.log(formValues)
     }
 
     render() {
         return ( 
-            <form>
-                <Field name="title" component={this.renderInput}/>
-                <Field name="description" component={this.renderInput}/>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field 
+                    name="title" 
+                    component={this.renderInput} 
+                    label="Enter Waifu Name"
+                />
+                <Field 
+                    name="description" 
+                    component={this.renderInput} 
+                    label="Describe your Waifu"
+                />
+                <button className="ui button primary">
+                    Submit
+                </button>
             </form>
         )
     }
 }
 
+const validate = (formValues) => {
+    const errors = {}
+
+    if (!formValues.title) {
+        errors.title = "You must enter your waifu's name."
+    }
+
+    if (!formValues.description) {
+        errors.description = "You must enter your waifu's description."
+    }
+
+    return errors
+}
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate)
