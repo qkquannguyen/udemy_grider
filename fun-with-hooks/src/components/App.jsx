d745a5fdbc9ea958c7046e2b28c591bd3cbc96d1
Quan Nguyen <qk.quannguyen@gmail.com>
2019-08-06 15:07:06 -0500

    add: react hooks implementation

--- fun-with-hooks/src/components/App.jsx
+++ fun-with-hooks/src/components/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-    return (
-        <div>
-            Hello
-            <h6>
-                World
-            </h6>
-        </div>
-    )
-}
+	const [resource, setResource] = useState("posts");
+
+	return (
+		<div>
+			<h2>{resource}</h2>
+			<button onClick={() => setResource("posts")}>Post</button>
+			<button onClick={() => setResource("todos")}>Todo</button>
+		</div>
+	);
+};
 
 export default App;
