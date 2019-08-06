d42cd569a8d00901005f2355691be908b5434ade
Quan Nguyen <qk.quannguyen@gmail.com>
2019-08-06 15:57:02 -0500

    add: posts button will do an axios call to jsonholder

--- fun-with-hooks/src/components/App.jsx
+++ fun-with-hooks/src/components/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
+import ResourceList from './ResourceList'
+
 const App = () => {
+    // --- NOTE: Calling setResource() will trigger a re-render
 	const [resource, setResource] = useState("posts");
 
 	return (
 		<div>
-			<h2>{resource}</h2>
+            <ResourceList resource={resource}/>
 			<button onClick={() => setResource("posts")}>Post</button>
-			<button onClick={() => setResource("todos")}>Todo</button>
+			<button onClick={() => setResource("todos")}>Todos</button>
 		</div>
 	);
 };
---
+++ fun-with-hooks/src/hooksNotes.js
@@ -0,0 +1,7 @@
+/**
+ * useState()       --> Allow a FC to use component-level state
+ * useEffect()      --> Allow FC to use 'lifecycle methods'
+ * useContext()     --> Allow a functional component to use the context system
+ * useRef()         --> Allow a FC to use the reff system
+ * useReducer()     --> Allow a FC to store data through a 'reducer'
+ */
