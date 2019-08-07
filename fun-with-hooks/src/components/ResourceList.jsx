29a8958a4e4f3fc7604708ae626ce1436630d684
Quan Nguyen <qk.quannguyen@gmail.com>
2019-08-07 08:34:07 -0500

    add: useEffect to ResourceList

--- fun-with-hooks/src/components/ResourceList.jsx
+++ fun-with-hooks/src/components/ResourceList.jsx
@@ -4,18 +4,16 @@ import axios from "axios";
 const ResourceList = ({ resource }) => {
 	const [resources, setResources] = useState([]);
 
-	const fetchResource = async resource => {
-		const response = await axios.get(
-			`https://jsonplaceholder.typicode.com/${resource}`
-		);
-
-		setResources(response.data);
-	};
-
 	// --- NOTE: useEffect() will essentially combine componentDidMount/componentDidUpdate together
 	useEffect(() => {
-		fetchResource(resource);
-	}, []);
+		(async resource => {
+			const response = await axios.get(
+				`https://jsonplaceholder.typicode.com/${resource}`
+			);
+
+			setResources(response.data);
+		})(resource);
+	}, [resource]);
 
 	return <div>{resources.length}</div>;
 };
